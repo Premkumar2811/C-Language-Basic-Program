9aea0f9657b410125c5509be17d8f01d12604597
Premkumar2811 <premkumarpatil093@gmail.com>
2025-01-19 22:42:54 +0530

    Create sum of any 2no.c

---
+++ sum of any 2no.c
@@ -0,0 +1,16 @@
+void main()
+{
+    int n1,n2,ans;
+    clrscr();
+
+    printf("\nEnetr first no");
+    scanf("%d,&n1");
+
+    printf("\nEnter second no");
+    scanf("%d,&n2");
+
+    ans=n1+n2;
+    printf("\nAdd=%d",ans);
+
+    getch();
+}
