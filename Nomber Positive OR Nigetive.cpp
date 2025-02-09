c7f3fa49e12f06319cef40074d61edf330ccba80
Premkumar Patil <premkumarpatil093@gmail.com>
2025-02-09 18:19:32 +0530

    Add files via upload

---
+++ Nomber Positive OR Nigetive.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x;
+	printf("\n Enter any no=");
+	scanf("%d",&x);
+	
+	if(x>0)
+	{
+		printf("\n Positive");
+	}
+	else
+	{
+		printf("\n Nigetive");
+	}
+	getch();
+}
